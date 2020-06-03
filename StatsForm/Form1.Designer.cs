5a557f25e2d54fb93431c30c24e20a670e7ba05b
Jared Lynch <52161844+ikoyhn@users.noreply.github.com>
2020-06-03 14:24:47 -0400

    Add project files.

---
+++ StatsForm/Form1.Designer.cs
@@ -0,0 +1,102 @@
+﻿namespace StatsForm
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.UserInput = new System.Windows.Forms.TextBox();
+            this.CalculateButton = new System.Windows.Forms.Button();
+            this.InputDescription = new System.Windows.Forms.Label();
+            this.ExitButton = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // UserInput
+            // 
+            this.UserInput.Location = new System.Drawing.Point(27, 70);
+            this.UserInput.Name = "UserInput";
+            this.UserInput.Size = new System.Drawing.Size(600, 20);
+            this.UserInput.TabIndex = 0;
+            this.UserInput.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            // 
+            // CalculateButton
+            // 
+            this.CalculateButton.Enabled = false;
+            this.CalculateButton.Location = new System.Drawing.Point(466, 135);
+            this.CalculateButton.Name = "CalculateButton";
+            this.CalculateButton.Size = new System.Drawing.Size(161, 78);
+            this.CalculateButton.TabIndex = 1;
+            this.CalculateButton.Text = "Calculate";
+            this.CalculateButton.UseVisualStyleBackColor = true;
+            this.CalculateButton.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // InputDescription
+            // 
+            this.InputDescription.AutoSize = true;
+            this.InputDescription.Location = new System.Drawing.Point(24, 54);
+            this.InputDescription.Name = "InputDescription";
+            this.InputDescription.Size = new System.Drawing.Size(206, 13);
+            this.InputDescription.TabIndex = 2;
+            this.InputDescription.Text = "Enter values of list (Separated by a space)";
+            this.InputDescription.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // ExitButton
+            // 
+            this.ExitButton.Location = new System.Drawing.Point(27, 229);
+            this.ExitButton.Name = "ExitButton";
+            this.ExitButton.Size = new System.Drawing.Size(75, 23);
+            this.ExitButton.TabIndex = 3;
+            this.ExitButton.Text = "Exit";
+            this.ExitButton.UseVisualStyleBackColor = true;
+            this.ExitButton.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.Window;
+            this.ClientSize = new System.Drawing.Size(655, 264);
+            this.Controls.Add(this.ExitButton);
+            this.Controls.Add(this.InputDescription);
+            this.Controls.Add(this.CalculateButton);
+            this.Controls.Add(this.UserInput);
+            this.Name = "Form1";
+            this.Text = "Five Number Summary Calculator";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox UserInput;
+        private System.Windows.Forms.Button CalculateButton;
+        private System.Windows.Forms.Label InputDescription;
+        private System.Windows.Forms.Button ExitButton;
+    }
+}
+
---
+++ StatsForm/Form1.cs
@@ -0,0 +1,124 @@
+﻿using Calculations;
+using FiveNumberSummary;
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace StatsForm
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            string Input = UserInput.Text;
+            ConvertInput.Input(Input);
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+            CalculateButton.Enabled = true;
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+    }
+}
+namespace FiveNumberSummary
+{
+    class ConvertInput
+    {
+        public static void Input(string userInput)
+        {
+            string[] strList = userInput.Split(',');
+            var nums = new double[strList.Length];
+            int totalNums = nums.Length;
+            for (int i = 0; i < totalNums; i++)
+            {
+                nums[i] = Convert.ToDouble(strList[i]);
+            }
+            Array.Sort(nums);
+            ;
+
+            double min = Operations.Min(nums);
+            double q1 = Operations.Percentile(nums, 25);
+            double medi = Operations.Percentile(nums, 50);
+            double q3 = Operations.Percentile(nums, 75);
+            double max = Operations.Max(nums, totalNums);
+
+            MessageBox.Show("MIN:" + Convert.ToString(min) + "  "+
+                            "Q1:" + Convert.ToString(q1) + "  " +
+                            "MEDIAN:" + Convert.ToString(medi) + "  " +
+                            "Q3:" + Convert.ToString(q3) + "  " +
+                            "MAX:" + Convert.ToString(max) + "  ");
+        }
+    }
+}
+
+namespace Calculations
+{
+    class Operations
+    {
+        public static double Percentile(double[] nums, int totalNums)
+        {
+            if (totalNums >= 100) return nums[nums.Length - 1];
+
+            double position = (nums.Length + 1) * totalNums / 100;
+            double leftNumber = 0.0d, rightNumber = 0.0d;
+
+            double n = totalNums / 100.0d * (nums.Length - 1) + 1.0d;
+
+            if (position >= 1)
+            {
+                leftNumber = nums[(int)Math.Floor(n) - 1];
+                rightNumber = nums[(int)Math.Floor(n)];
+            }
+            else
+            {
+                leftNumber = nums[0]; // first data
+                rightNumber = nums[1]; // first data
+            }
+
+            //if (leftNumber == rightNumber)
+            if (Equals(leftNumber, rightNumber))
+                return leftNumber;
+            double part = n - Math.Floor(n);
+            return leftNumber + part * (rightNumber - leftNumber);
+        }
+
+        public static double Min(double[] nums)
+        {
+            double min = nums[0];
+            return min;
+        }
+
+        public static double Max(double[] nums, int totalNums)
+        {
+            double max = nums[totalNums - 1];
+            return max;
+        }
+    }
+}
